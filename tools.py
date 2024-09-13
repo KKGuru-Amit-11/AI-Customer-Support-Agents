54991656cd803065a568842fe1b01a75b7c9c63d
Amit Kumar <142104891+KKGuru-Amit-11@users.noreply.github.com>
2024-09-13 23:18:55 +0530

    tools.py

--- tools.py
+++ tools.py
@@ -3,7 +3,7 @@ import os
 import streamlit as st
 from crewai_tools import SerperDevTool,ScrapeWebsiteTool
 
-os.environ['SERPER_API_KEY'] = st.secrets['SERPER_API_KEY']
+os.environ['SERPER_API_KEY'] = '5e91bacd42a33cdf4299197ce6d7e49aaca23310'
 
 search_tool=SerperDevTool()
 scrape_tool=ScrapeWebsiteTool(website_url='https://techshiney.com/')
