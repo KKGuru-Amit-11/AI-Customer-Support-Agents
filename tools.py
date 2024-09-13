7da6dc6880be03b2a9b4172c0e4ef798c79ea7a7
Amit Kumar <142104891+KKGuru-Amit-11@users.noreply.github.com>
2024-09-13 12:31:56 +0530

    tools.py

--- tools.py
+++ tools.py
@@ -6,4 +6,4 @@ from crewai_tools import SerperDevTool,ScrapeWebsiteTool
 os.environ['SERPER_API_KEY'] = st.secrets['SERPER_API_KEY']
 
 search_tool=SerperDevTool()
-scrape_tool=ScrapeWebsiteTool(website_url='https://techshiney.com/')
+scrape_tool=ScrapeWebsiteTool(website_url='https://www.cetpainfotech.com/')
