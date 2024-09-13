334967adc155fa1aa0b51d1706517116bbf121d8
Amit Kumar <142104891+KKGuru-Amit-11@users.noreply.github.com>
2024-09-13 10:37:25 +0530

    tasks.py

--- tasks.py
+++ tasks.py
@@ -7,7 +7,7 @@ inquiry_resolution = Task(
     {customer} is the one that reached out. make sure to use everything 
     you know to provide the best support possile. you must strive to provide a complete 
     and accurate response to the customer's inquiry''',
-    expected_output='''A detailed,informative response to the custome's inquiry that 
+    expected_output='''A detailed,informative and short response to the custome's inquiry that 
     addresses all aspects of their question. The response should include references 
     to everything external data or solutions ensure the answer is complete leaving no 
     questions unanswered, and maintain a helpful and friendly tone throughout''',
@@ -21,7 +21,7 @@ quality_assurance_review = Task(
     customer's inquiry have been addressed throughly with a helpful and friendly tone. check 
     for references and sources used to find the information, ensuring the response is well-support 
     and leaves no questions unanswered for their inquiry.''',
-    expected_output='''A Final, detailed, and infomative response ready to  be send to the customer
+    expected_output='''A Final, detailed, informative and short to the point response ready to be send to the customer
     This response should fully addess the customer's inquiry, incorporating all relevant 
     feadback and  improvment. don't be too formal, we are a chill and cool company but 
     maintain a professional and friendly tone throughout''',
