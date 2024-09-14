1fbfb3c05724e06f140a7be90059b0d072ef9409
Amit Kumar <142104891+KKGuru-Amit-11@users.noreply.github.com>
2024-09-14 13:20:01 +0530

    tasks.py

--- tasks.py
+++ tasks.py
@@ -7,10 +7,10 @@ inquiry_resolution = Task(
     {customer} is the one that reached out. make sure to use everything 
     you know to provide the best support possible. you must strive to provide a complete 
     and accurate response to the customer's inquiry''',
-    expected_output='''A detailed, informative and response to the customer's enquiry that 
+    expected_output='''A detailed, informative and response to the customer's inquiry that 
     addresses all aspects of their question. The response should include references 
     to everything external data or solutions ensure the answer is complete leaving no 
-    questions unanswered, and maintain a helpful and friendly tone throughout''',s
+    questions unanswered, and maintain a helpful and friendly tone throughout'''
     agent=support_agent
 )
 
@@ -20,7 +20,7 @@ quality_assurance_review = Task(
     the high-quality standards expected for customer support. Verify that all parts of the 
     customer's inquiry have been addressed throughly with a helpful and friendly tone. check 
     for references and sources used to find the information, ensuring the response is well-support 
-    and leaves no questions unanswered for their enquiry.''',
+    and leaves no questions unanswered for their inquiry.''',
     expected_output='''A Final, detailed, informative and to the point response ready to be send to the customer
     This response should fully address the customer's inquiry, incorporating all relevant 
     feedback and  improvment. don't be too formal, we are a chill and cool company but 
